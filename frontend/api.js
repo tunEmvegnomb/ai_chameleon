df25e636be50b4cf511a4e75f4e14c6bfac9de97
migdracios <migdracios@gmail.com>
2022-05-22 17:00:40 +0900

    시도 - 셀피를 보내고 받아오는 과정 찍어보기

--- frontend/api.js
+++ frontend/api.js
@@ -8,5 +8,13 @@ async function uploadSelfie() {
     form_data.append("file_give", selfieData)
     console.log(form_data.get("file_give"))
 
+    const response = await fetch('http://127.0.0.1:5000/saveselfie', {
+        method: 'POST',
+        body: JSON.stringify(selfieData)
+    })
+
+    console.log('셀피를 보내고 받아옵시다')
+    response_json = await response.json()
+    console.log(response_json)
 
 }
