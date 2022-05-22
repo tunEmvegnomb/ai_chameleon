868ac64374276fca112a534fd1c468b46d926971
migdracios <migdracios@gmail.com>
2022-05-22 16:55:16 +0900

    추가 - 이미지 파일 받아서 폼 데이터에 저장하기

--- frontend/api.js
+++ frontend/api.js
@@ -3,4 +3,10 @@ console.log('api script on load')
 async function uploadSelfie() {
     const selfieData = document.getElementById("file").files[0]
     console.log(selfieData)
+
+    let form_data = new FormData()
+    form_data.append("file_give", selfieData)
+    console.log(form_data.get("file_give"))
+
+
 }
